12709e637705ec295d7f97024b2d43d0092c1d3c
Saebyeol Ju <n2wstar@gmail.com>
2022-06-07 21:23:17 +0900

    🚧 sass 기본 스타일 수정, Main page 작업

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,25 +1,43 @@
+import { useEffect, useState } from "react";
 import * as AiIcons from "react-icons/ai";
 import * as IoIcons from "react-icons/io";
-import "../css/styles.css";
 
 export default function Main() {
+  const [ingredient, setIngredient] = useState("");
+
+  const handleSearchInput = (e: React.ChangeEvent<any>) => {
+    setIngredient(e.target.value);
+  };
+
   return (
-    <>
-      <a>
-        <AiIcons.AiFillGithub />
-      </a>
-      <a>
-        <IoIcons.IoMdGlobe />
-      </a>
-      <div>
-        <img className="logo-img" src="../assets/img/logo.png" />
-        <h1 className="logo --ff-main">김치 있니?</h1>
+    <div className="container">
+      <div className="logo_box">
+        <img
+          className="logo-img"
+          src="https://cdn-icons-png.flaticon.com/512/4727/4727309.png"
+          alt="logo"
+        />
+        <h1 className="logo --ff-main text-white">김치있니?</h1>
       </div>
-      <div className="flex">
-        <input value="김치재료를 입력하세요" className="text-grey"></input>
+      <div className="input_box center bg-white">
         <AiIcons.AiOutlineSearch />
+        <input 
+          type="search"
+          placeholder="김치 재료를 입력하세요."
+          className="text-grey"
+          value={ingredient}
+          onSubmit={handleSearchInput}
+        ></input>
       </div>
-      <button className="btn">O / X 게임</button>
-    </>
+      <button className="btn text-white">O / X 게임</button>
+      <div className="icon_box">
+        <a>
+          <IoIcons.IoMdGlobe className="text-white" />
+        </a>
+        <a href="https://github.com/SaebyeolJu/find-kimchi">
+          <AiIcons.AiFillGithub className="text-white" />
+        </a>
+      </div>
+    </div>
   );
 }
