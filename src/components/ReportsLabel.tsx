5736730e1ae9b1026236d490b4d76a8e18a468bc
Saebyeol Ju <n2wstar@gmail.com>
2022-06-10 14:24:21 +0900

    🚧Report 페이지 리팩토링

--- src/components/ReportsLabel.tsx
+++ src/components/ReportsLabel.tsx
@@ -1,16 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import * as AiIcons from "react-icons/ai";
 
-const ReportsField = (props: any) => {
+const ReportsLabel = (props: any) => {
   return (
     <>
       <div className="reports__label-field">
         <AiIcons.AiOutlineInfoCircle className="reports__Info-icon" />
         <p className="reports__label">{props.label}</p>
       </div>
-      <input className="reports__input" name="메모" type="text" />
     </>
   );
 };
 
-export default ReportsField;
+export default ReportsLabel;
--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,15 +1,73 @@
-import React from "react";
-import ReportsField from "../components/ReportsField";
+import React, { useState } from "react";
+import ReportsLabel from "../components/ReportsLabel";
 
 const Report = () => {
+  const [form, setForm] = useState({
+    kimchiName: "",
+    link: "",
+    memo: "",
+  });
+
+  const { kimchiName, link, memo } = form;
+
+  const handleSubmit = (e: any) => {
+    if (memo === "") {
+      alert("입력을 확인해주세요");
+    } else {
+      alert("소중한 의견 감사합니다!");
+    }
+    e.preventDefault();
+  };
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+    console.log(form);
+  };
+
+  const onReset = () => {
+    setForm({
+      kimchiName: "",
+      link: "",
+      memo: "",
+    });
+  };
+
   return (
     <div className="container">
-      <form className="reports">
+      <form className="reports" onSubmit={handleSubmit}>
         <h1 className="reports__title">제보하기</h1>
-        <ReportsField label="김치" />
-        <ReportsField label="링크" />
-        <ReportsField label="메모" />
-        <button className="btn" type="submit">
+        <ReportsLabel label="김치 종류" />
+        <input
+          className="reports__input"
+          value={form.kimchiName}
+          placeholder="김치 이름을 남겨주세요"
+          onChange={handleChange}
+          name="kimchiName"
+          type="text"
+        />
+        <ReportsLabel label="링크" />
+        <input
+          className="reports__input"
+          value={form.link}
+          placeholder="참고 링크를 남겨주세요"
+          onChange={handleChange}
+          name="link"
+          type="text"
+        />
+        <ReportsLabel label="메모" />
+        <input
+          className="reports__input"
+          value={form.memo}
+          placeholder="메모를 남겨주세요"
+          onChange={handleChange}
+          name="memo"
+          type="text"
+        />
+        <button className="btn" onClick={onReset} type="submit">
           확인
         </button>
       </form>
