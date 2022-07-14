5dd611113a99fabefe68b80631710cd2f751458e
Saebyeol Ju <saebyeolju768@gmail.com>
2022-07-14 15:32:33 +0900

    useAsync 적용 중

--- src/pages/SearchResult.tsx
+++ src/pages/SearchResult.tsx
@@ -1,38 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import axios from "../api/axios";
+import useAsync from "../hooks/useAsync";
+
 import HomeButton from "../components/HomeButton";
 import ReportLink from "../components/ReportLink";
 
 const SearchResult = () => {
-  let { searchWord } = useParams();
-  const [Info, setInfo] = useState("");
-  // escaping 처리
+  const [state, refetch] = useAsync(getSearchData, [], true);
+  const { loading, data: info, error } = state<any[]>([]);
+  let { searchWord }: any = useParams();
+  const decodedWord = decodeURI(searchWord);
+
+  async function getSearchData(decodedWord: any) {
+    const request = await axios.get(`kimchi/${decodedWord}`);
+    return request.data;
+  }
+
+  // const { loading, data: info, error } = useAsync(SearchResult);
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다.</div>;
+  if (!info) return null;
 
   useEffect(() => {
-    // fetch 함수를 쓴다면 이렇게 json 가져올 수 있음
-    // fetch(`http://localhost:5000/kimchi/${searchWord}`)
-    //   .then((res) => console.log(res.text()))
-    //   .then((actualData) => console.log(actualData));
-
-    async function fetchData() {
-      const request = await axios.get(`/${searchWord}`);
-      console.log(request);
-      setInfo(request.data.results);
-      return request;
-    }
-    fetchData();
-  }, [searchWord]);
+    /*
+    fetch 함수를 쓴다면 이렇게 json 가져올 수 있음
+    fetch(`http://localhost:5000/kimchi/${searchWord}`)
+      .then((res) => console.log(res.text()))
+      .then((actualData) => console.log(actualData));
+    */
+    getSearchData(decodedWord);
+  }, [decodedWord]);
 
   return (
     <div className="container">
       <div className="search-box">
-        <h1 className="search__name">{searchWord} 김치</h1>
-        <img
-          className="search__img"
-          src="https://img.koreatimes.co.kr/upload/newsV2/images/202106/88dbf0524fd74259b2d00f6cd7341349.jpg/dims/resize/740/optimize"
-          alt="search-img"
-        />
+        <h1 className="search__name">{decodedWord} 김치</h1>
+        <img className="search__img" src={""} alt="search-img" />
         <p>이런..! 한국인 맞으신가요?</p>
       </div>
       <HomeButton />
