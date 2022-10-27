1a1a7e10a2a58d18a3396e2b24ef5a2a02ef393b
Saebyeol Ju <saebyeolju768@gmail.com>
2022-10-27 09:33:44 +0900

    ⚡️ Main page 검색 기능 향상(영/한, 빈문자 검사)

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -13,11 +13,33 @@ export const Main: React.FC = () => {
 
   const [searchWord, setSearchWord] = useState("");
 
+  /**
+   * @description - 입력받은 검색어 영/한으로만 이루어져있는지 확인 후 엔터를 누르면 검색 결과 페이지로 이동
+   * @param e - 키보드 이벤트
+   * @returns - 검색 결과 페이지로 이동
+   * @see SearchResult.tsx
+   *
+   */
   const handleSubmit = async (e: React.FormEvent) => {
+    // 검색어가 한글, 영어로만 이루어져있는지 확인
+    const regExp = /^[ㄱ-ㅎ|가-힣|a-z|A-Z\s]+$/;
     e.preventDefault();
-    navigate(`/kimchi/${searchWord}`);
+
+    if (regExp.test(searchWord)) {
+      // 단어 사이 공백은 제거
+      searchWord.replace(/\s/g, "")
+        ? navigate(`/kimchi/${searchWord.replace(/\s/g, "")}`)
+        : alert("검색어를 입력해주세요");
+      setSearchWord("");
+    } else {
+      alert("한글, 영어로만 입력해주세요");
+    }
   };
 
+  /**
+   * @description - 검색어를 입력하면 검색어를 상태로 저장
+   * @param e - 키보드 이벤트
+   */
   const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchWord(e.target.value);
   };
@@ -34,6 +56,7 @@ export const Main: React.FC = () => {
         </a>
         <h1 className={["logo__title", "txt-white"].join(" ")}>김치있니?</h1>
       </div>
+      {/* 검색창 */}
       <form className="search bg-white" onSubmit={handleSubmit}>
         <AiIcons.AiOutlineSearch className="search__icon" color="black" />
         <input
@@ -50,12 +73,12 @@ export const Main: React.FC = () => {
         btn_dest="game"
       />
       <div className={["icon-section", "txt-white"].join(" ")}>
-        {/* 언어 설정 */}
+        {/* 언어 설정 버튼 */}
         <a className="icon">
           <IoIcons.IoMdGlobe />
         </a>
 
-        {/* git link */}
+        {/* git repository로 가는 link */}
         <a
           className="icon"
           href="https://github.com/SaebyeolJu/find-kimchi"
