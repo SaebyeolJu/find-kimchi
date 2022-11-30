56acb6a467d624f4b535e737a80d8e0c7bf8fc12
Saebyeol Ju <saebyeolju768@gmail.com>
2022-11-30 20:47:20 +0900

    검색결과 애니메이션 추가

--- src/pages/SearchResult.tsx
+++ src/pages/SearchResult.tsx
@@ -1,35 +1,26 @@
-import { useEffect, useCallback } from "react";
+import { useEffect, useCallback, useState } from "react";
 import { useParams } from "react-router";
-import { useAsync } from "react-async";
 import axios from "../api/axios";
 
 import NoResult from "./NoResult";
 import NavigateBtn from "../components/NavigateBtn";
 import ReportLink from "../components/ReportLink";
+import CategoryIcon from "../components/CategoryIcon";
+
+import Button from "../components/Button";
 
 const SearchResult = () => {
   let { searchWord }: any = useParams();
   const decodedWord = decodeURI(searchWord);
+  const [searchResult, setSearchResult] = useState();
 
-  const fetchAndSetInfo = useCallback(async () => {
+  const fetchInfo = useCallback(async () => {
     const URL = `kimchi/${decodedWord}`;
     const request = await axios.get(URL);
 
-    return request.data;
+    setSearchResult(request.data.data[0]);
+    return request.data.data[0];
   }, [decodedWord]);
-
-  const {
-    data: results,
-    error,
-    isLoading,
-  } = useAsync({
-    promiseFn: fetchAndSetInfo,
-    watch: decodedWord,
-  });
-
-  // 주의
-  // useEffect 는 항상 함수 반환함 -> async await를 잘못 쓰면 promise 객체를 반환해서 계속 반환함
-  // https://velog.io/@he0_077/useEffect-훅에서-async-await-함수-사용하기
 
   useEffect(() => {
     /*
@@ -38,38 +29,74 @@ const SearchResult = () => {
       .then((res) => console.log(res.text()))
       .then((actualData) => console.log(actualData));
     */
-    fetchAndSetInfo();
+    fetchInfo();
   }, [decodedWord]);
 
-  if (isLoading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다.</div>;
-  if (!results.data[0]) return <NoResult searchWord={searchWord} />;
+  // 주의
+  // useEffect 는 항상 함수 반환함 -> async await를 잘못 쓰면 promise 객체를 반환해서 계속 반환함
+  // https://velog.io/@he0_077/useEffect-훅에서-async-await-함수-사용하기
+
+  if (!searchResult || !searchResult.is_kimchi)
+    return <NoResult searchWord={searchWord} />;
 
   return (
-    <div className="container">
-      <div className="result">
-        <h1 className={["search__name", "txt-white"].join(" ")}>
-          {decodedWord} 김치
-        </h1>
-        <a href={results.data[0]?.source} target="_blank" rel='target="_blank"'>
-          <img
-            className="result__img"
-            src={results.data[0]?.img_link}
-            width={300}
-            alt="search-img"
-          />
-        </a>
-        <p className={["result__comment", "txt-white"].join(" ")}>
-          {results.data[0]?.comment}
-        </p>
+    <div className="result">
+      <div className="result__header">
+        <img
+          className="result__header-img"
+          src={`/img/scroll/scroll_folded.png`}
+          alt="scroll"
+        />
+        <img
+          className="result__header-img"
+          src={`/img/scroll/scroll_folded.png`}
+          alt="scroll"
+        />
       </div>
-      <NavigateBtn btn_txt="처음으로" btn_type="btn--prime" btn_dest="" />
-      <button className={["btn--second", "txt-white"].join(" ")}>
-        <a href={results.data[0]?.source} target="_blank" rel='target="_blank"'>
-          레시피 보러가기
-        </a>
-      </button>
-      <ReportLink />
+      <div className="result__card">
+        <div className="result__sidebar"></div>
+        <div className="result__content">
+          <CategoryIcon category={searchResult?.ingredient_categories} />
+          <h1
+            className={["result__name", "txt-white", "bg-dark-brown"].join(" ")}
+          >
+            {decodedWord} 김치
+          </h1>
+          <h2 className={["result__title", "txt-dark", "italic"].join(" ")}>
+            {searchResult?.kimchi_title}
+          </h2>
+          <a
+            href={searchResult?.recipe_link}
+            target="_blank"
+            rel='target="_blank"'
+          >
+            <img
+              className="result__img"
+              src={searchResult?.recipe_pic}
+              width={300}
+              alt="search-img"
+            />
+          </a>
+          <p className={["result__comment", "txt-dark"].join(" ")}>
+            {searchResult?.kor_comment}
+          </p>
+          <div className="buttons">
+            <NavigateBtn btn_txt="처음으로" btn_type="btn--prime" btn_dest="" />
+            <button className={["btn--second", "txt-dark"].join(" ")}>
+              <a
+                href={searchResult?.recipe_link}
+                target="_blank"
+                rel='target="_blank"'
+              >
+                레시피 보러가기
+              </a>
+            </button>
+          </div>
+          <div className="result__side"></div>
+          <ReportLink />
+        </div>
+        <div className="result__sidebar"></div>
+      </div>
     </div>
   );
 };
